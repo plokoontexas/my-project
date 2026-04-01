9cd107c239cd9d29254602bbaf7388e2822e9f20
Jack Powers <jackpowers@macbookpro.ddns.uark.edu>
2026-04-01 10:43:55 -0500

    updates to slurm

--- Scripts/write_slurm.py
+++ Scripts/write_slurm.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
 
-# prompt the user for their position
-position = int(input("Please enter a position in the Fibonacci sequence: "))
+import argparse
 
-# initialize 2 integers
-a, b = 0, 1
+parser = argparse.ArgumentParser(description="This script adds the intial Slurm info")
 
-for i in range(position):
-    a, b = b, a + b
+parser.add_argument("job_name", help="Name of the Job")
 
-fibonacci_number = a
+parser.add_argument("hours", help="Number of Hours", type=int)
 
-print(f"The Fibonacci number for {position} is {fibonacci_number}")
+parser.add_argument("que", help="Which que do you want to use")
+
+email = "jp297@uark.edu"
+
+args = parser.parse_args()
+
+print("#!/bin/bash")
+print(f"#SBATCH --job-name={args.job_name}")
+print(f"#SBATCH --time={args.hours}:00:00")
+print(f"#SBATCH --partition {args.que}")
+print("#SBATCH -q cloud")
+print("#SBATCH -N 1")
+print("#SBATCH -n 1")
+print("#SBATCH -c 1")
+print(f"#SBATCH --mail-user={email}")
+print("")
+print("module load blast")
+print("")
+print("blastn -query watermelon.fsa -subject watermelon.fsa > wat.vs.wat.blastn")
+
+
 
